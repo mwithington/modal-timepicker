11463cb61c76489fcbf7faabd866406d9c0abb8a
mwithington <withingtonm@wit.edu>
2015-06-02 10:10:48 -0400

    svaes

---
+++ WebApplication1/WebApplication1/fullcalendar-2.3.1/demos/app.js
@@ -0,0 +1,41 @@
+//code for day click modal
+var mymodal = angular.module('mymodal', []);
+
+mymodal.controller('MainCtrl', function ($scope) {
+    $scope.showModal = false;
+    $scope.buttonClicked = "";
+    $scope.toggleModal = function (btnClicked) {
+        $scope.buttonClicked = btnClicked;
+        $scope.showModal = !$scope.showModal;
+    };
+});
+
+mymodal.directive('modal', function () {
+    return {
+        templateUrl:'userOptionsModal.html' ,
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: true,
+        link: function postLink(scope, element, attrs) {
+            scope.$watch(attrs.visible, function (value) {
+                if (value == true)
+                    $(element).modal('show');
+                else
+                    $(element).modal('hide');
+            });
+
+            $(element).on('shown.bs.modal', function () {
+                scope.$apply(function () {
+                    scope.$parent[attrs.visible] = true;
+                });
+            });
+
+            $(element).on('hidden.bs.modal', function () {
+                scope.$apply(function () {
+                    scope.$parent[attrs.visible] = false;
+                });
+            });
+        }
+    };
+});
