c4d9124287cdfd2f478db288f6c228c412d5f779
mwithington <withingtonm@wit.edu>
2015-06-04 11:08:52 -0400

    function add to main controller

--- WebApplication1/WebApplication1/fullcalendar-2.3.1/demos/app.js
+++ WebApplication1/WebApplication1/fullcalendar-2.3.1/demos/app.js
@@ -7,8 +7,61 @@ mymodal.controller('MainCtrl', function ($scope) {
     $scope.toggleModal = function (btnClicked) {
         $scope.buttonClicked = btnClicked;
         $scope.showModal = !$scope.showModal;
+
+
     };
+
+    $scope.hoursCalc = function () {
+        var sTime24;
+
+        var eTime24;
+
+        var x = document.getElementById("Start").value;
+
+        var y = document.getElementById("End").value;
+
+
+        console.log(x, "This is start time")
+        console.log(y, "This is end Time")
+        var eTime = y.split(":", 1)
+
+        var sTime = x.split(":", 1)
+        console.log(eTime)
+
+        var isAM = x[5];
+        var isPM = y[5];
+        console.log(isAM)
+        console.log(isPM)
+        switch (isPM) {
+          case "P":
+             eTime24 = parseInt(eTime, 10) + 12;
+          break;
+          default:
+             eTime24 = parseInt(eTime, 10);
+        };
+        switch (x[6]) {
+         case "P":
+           sTime24 = parseInt(sTime, 10) + 12;
+           break;
+         default:
+           sTime24 = parseInt(sTime, 10);
+        };
+        console.log(eTime24);
+
+        var z = eTime24 - sTime24;
+
+        console.log(z);
+
+
+        
+
+    }
 });
+
+
+
+
+
 
 mymodal.directive('modal', function () {
     return {
@@ -70,8 +123,12 @@ mymodal.directive('fullCalendar', function () {
                     
                     
                     var eventData;
+                    
                     var x = document.getElementById("Start").value;
                     var y = document.getElementById("End").value;
+
+                    console.log(x)
+
                     //var modalHide = $('#myModal').modal("hide");
                     var eventData;
                     if (modalShow) {
@@ -132,4 +189,5 @@ mymodal.directive('fullCalendar', function () {
 
 
 });
-   
+
+
